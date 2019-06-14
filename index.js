b929b20768904951dea3b506149ebf7c64bf0ad4
Harish Mathanan <harish.mathanan@hotmail.com>
2019-06-14 11:15:33 +0800

    Add configuration file Create user sign in route and functionality Create user sign up route and functionality

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 5555;
 
+const userRoutes = require('./routes/user');
+
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Home
 app.get('/', (req, res) => {
   res.status(200).send('MERN Authentication Example')
 });
+
+// Routes
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, (error) => {
   if (error) {
---
+++ routes/user.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const Config = require('../config');
+
+// User registration
+router.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation - required fields
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'All fields are requires.' });
+  }
+ 
+  let salt = await bcrypt.genSalt(10);
+  let hash = await bcrypt.hash(salt, password);
+
+  const newUser = new User({
+    name,
+    email,
+    password: hash
+  });
+
+  try {
+    const user = await newUser.save();
+    const token = await jwt.sign({ id: user.id }, Config.JWT_SECRET, { expiresIn: '1h' });
+
+    return res.cookie('token', token).sendStatus(200);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'Server error.' });
+  }
+});
+
+// User login
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validation - required fields
+  if (!email || !password) {
+    return res.status(400).json({ message: 'All fields required.' });
+  }
+
+  // Validation - user doesn't exist
+  let user = await User.find({ email });
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' });
+  }
+
+  // Validation - invalid credentials
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Invalid credentials.' });
+  }
+
+  try {
+    let token = await jwt.sign({ id: user.id }, Config.JWT_SECRET, { expiresIn: '1h' });
+
+    res.cookie('token', token).sendStatus(200);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'Server error.' });
+  }
+});
