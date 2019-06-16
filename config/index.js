c9e930a5a8a6378cc942dbd6d583ed490f489b1b
Harish Mathanan <harish.mathanan@hotmail.com>
2019-06-16 19:24:23 +0800

    Update config file

--- config/index.js
+++ config/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://mdb-user:22Silentmind@clusterhmdev-ejk2p.mongodb.net/test?retryWrites=true&w=majority',
-  //MONGODB_URI: 'MongoDbConnectionString',
+  MONGODB_URI: 'MongoDbConnectionString',
   JWT_SECRET: 'JsonWebTokenSecret'
 };
